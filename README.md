86c72dbf282596bf8fad0751d6484f68befd344d
Kevin Mitchell <loudwinston@gmail.com>
2014-07-26 18:08:22 -0400

    Cleanup for better starting point

--- README.md
+++ README.md
@@ -1,14 +1,26 @@
 node-travel-student
 ===================
 
-#Steps to set up git
-git config --global user.email "you@example.com"
-git config --global user.name "Your Name"   
+npm install
 
-git add -A
-git commit -a -m "your commit message here"
-git push origin master
-	will ask for github credentials
-heroku login
-	will ask for heroku credentials
+#Steps to set up git and heroku
+	git config --global user.email "you@example.com"
+	git config --global user.name "Your Name"   
 
+	git add -A
+	git commit -a -m "your commit message here"
+	
+	# NOTE: this will ask for github credentials
+	git push origin master 
+		
+	# NOTE: will ask for heroku credentials
+	heroku login
+		
+	heroku keys:add
+	heroku create your-app-name-here
+
+	# NOTE: type 'yes' if asked about RSA key
+	git push heroku master
+		
+
+	
--- app.js
+++ app.js
@@ -1,43 +1,14 @@
 var appHelper = require("./apphelper.js");
 
 appHelper.runApp(function(app, db) {
-	console.log("My app is running!");
+	
 
-	app.get("/hello", function (req, resp) {
-		resp.write("HELLO WORLD! My name is Kevin!");
-		resp.end();
-	});
+	console.log("Travel application is running!");
 
-
-	app.get("/html", function (req, resp) {
-		resp.write("<html><body><h1>Woo! Node is cool!</h1><button>Click me</button></body></html>");
-		resp.end();
-	});
-
-	app.get("/template/:name/:food/", function (req, resp) {
-		resp.render("template", { name: req.params.name, food: req.params.food });
-	});
-
+	var myName = "YOUR_NAME_HERE";
 	app.get("/", function (req, resp) {
-		
-		db.findArray({}, function(arr) {
-			resp.render("index", { locations: arr });
-		});
-
-		
+		resp.write("<html><body><h1>Welcome to "+myName+"'s first web app!</body></html>");
+		resp.end();
 	});
-
-	app.get("/rating/:rating", function (req, resp) {
-		var rating = parseInt(req.params.rating);
-		db.findArray({ rating: rating }, function(arr) {
-			resp.render("index", { locations: arr });
-		});
-
-		
-	});
-
-
-
-
 
 })
